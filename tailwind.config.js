c002d85a19cb11fc6eee76f49e46049999b69d59
ecomptonn <162512945+ecomptonn@users.noreply.github.com>
2025-03-26 13:00:07 -0700

    Updated project to use dm serif display and dm sans font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                serif: ['"DM Serif Display"', "serif"],
+                sans: ['"DM Sans"', "sans-serif"],
+            },
+        },
     },
     plugins: [],
 };
